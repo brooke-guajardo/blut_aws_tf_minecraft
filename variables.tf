d7ac657a0bd756e89b580dd1471c7e7a4d33287c
Brooke Guajardo <102095955+brooke-guajardo@users.noreply.github.com>
2023-11-06 14:36:45 -0800

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "cpu" {
+  description = "default amount of CPU to give to ecs"
+  value = 1024
+}
+
+variable "memory" {
+  description = "default amount of CPU to give to ecs"
+  value = 8192
+}
