ad1b957fe3d9aa47386a6da6ab315278188b42c4
brooke-guajardo <brooke.guajardo@gmail.com>
2023-11-09 20:00:32 -0600

    BUMP IT UP

--- ecs/variables.tf
+++ ecs/variables.tf
@@ -1,11 +1,11 @@
 variable "cpu" {
   description = "default amount of CPU to give to ecs"
-  default = 4096
+  default = 8192
 }
 
 variable "memory" {
   description = "default amount of memory to give to ecs"
-  default = 8192
+  default = 16384
 }
 
 variable "memory_env_var" {
