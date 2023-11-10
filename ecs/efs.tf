dd8d465e74c4c0be8ea349e39e2de900e17cf9a0
brooke-guajardo <brooke.guajardo@gmail.com>
2023-11-10 11:28:06 -0600

    BIG THINK

--- ecs/efs.tf
+++ ecs/efs.tf
@@ -1,4 +1,6 @@
-resource "aws_efs_file_system" "minecraft_efs" {}
+resource "aws_efs_file_system" "minecraft_efs" {
+    creation_token = "minecraft"
+}
 
 resource "aws_efs_mount_target" "minecraft_mount_1" {
    file_system_id  = aws_efs_file_system.minecraft_efs.id
