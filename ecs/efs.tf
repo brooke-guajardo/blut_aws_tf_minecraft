cc4fa04dd8000881724ca9bce2bbd97a84be09d9
brooke-guajardo <brooke.guajardo@gmail.com>
2023-11-09 21:43:52 -0600

    typos

--- ecs/efs.tf
+++ ecs/efs.tf
@@ -7,7 +7,7 @@ resource "aws_efs_mount_target" "mount" {
 
 resource "aws_efs_mount_target" "minecraft_mounts" {
    count = "length(module.vpc.public_subnets)"
-   file_system_id  = "${aws_efs_file_system.magento-efs.id}"
+   file_system_id  = "${aws_efs_file_system.minecraft_efs.id}"
    subnet_id = "${element(module.vpc.public_subnets, count.index)}"
    security_groups = ["${aws_security_group.minecraft_sg.id}"]
 }
