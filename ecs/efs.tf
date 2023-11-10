f99fe13ce8f6e81f5d5d8d9dfd7058ae06f85aa1
brooke-guajardo <brooke.guajardo@gmail.com>
2023-11-10 09:54:50 -0600

    nothing fancy

--- ecs/efs.tf
+++ ecs/efs.tf
@@ -5,9 +5,8 @@ resource "aws_efs_mount_target" "mount" {
   subnet_id      = module.vpc.public_subnets
 }
 
-resource "aws_efs_mount_target" "minecraft_mounts" {
-   count = "length(module.vpc.public_subnets)"
-   file_system_id  = "${aws_efs_file_system.minecraft_efs.id}"
-   subnet_id = "${element(module.vpc.public_subnets, count.index)}"
-   security_groups = ["${aws_security_group.minecraft_sg.id}"]
+resource "aws_efs_mount_target" "minecraft_mount_1" {
+   file_system_id  = aws_efs_file_system.minecraft_efs.id
+   subnet_id = module.vpc.public_subnets[0]
+   security_groups = data.aws_security_group.minecraft_sg.id
 }
