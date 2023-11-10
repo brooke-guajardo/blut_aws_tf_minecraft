d9f001bc67e275c12a1c46145f09f76ddb75bdcc
brooke-guajardo <brooke.guajardo@gmail.com>
2023-11-10 09:57:36 -0600

    one az at a time

--- ecs/efs.tf
+++ ecs/efs.tf
@@ -1,12 +1,12 @@
 resource "aws_efs_file_system" "minecraft_efs" {}
 
-resource "aws_efs_mount_target" "mount" {
+resource "aws_efs_mount_target" "mount_1" {
   file_system_id = aws_efs_file_system.minecraft_efs.id
-  subnet_id      = module.vpc.public_subnets
+  subnet_id      = module.vpc.public_subnets[0]
 }
 
 resource "aws_efs_mount_target" "minecraft_mount_1" {
    file_system_id  = aws_efs_file_system.minecraft_efs.id
    subnet_id = module.vpc.public_subnets[0]
-   security_groups = data.aws_security_group.minecraft_sg.id
+   security_groups = [aws_security_group.minecraft_sg.id]
 }
