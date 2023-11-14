f2b57256c1a4a6d8c52e07fe893982279bcb3f9f
brooke-guajardo <brooke.guajardo@gmail.com>
2023-11-14 10:32:39 -0600

    pain pain

--- discord/lambda_function.py
+++ discord/lambda_function.py
@@ -14,7 +14,8 @@ def lambda_handler(event, context):
 
     signature = event['headers']['x-signature-ed25519']
     timestamp = event['headers']['x-signature-timestamp']
-    body = event['body'].decode("utf-8")
+    body = event['body']
+    print(body)
 
     try:
         verify_key.verify(f'{timestamp}{body}'.encode(), bytes.fromhex(signature))
