5d8e558b6e7f897ccb2f34b1efbc5deeee3594ba
brooke-guajardo <brooke.guajardo@gmail.com>
2023-11-14 10:56:04 -0600

    jsooooon

--- discord/lambda.tf
+++ discord/lambda.tf
@@ -10,6 +10,11 @@ resource "aws_lambda_function" "blut_aws_tf_minecraft_lambda" {
   source_code_hash = filebase64sha256("${path.module}/deployment_package.zip")
 
   role = aws_iam_role.lambda_exec.arn
+  environment {
+    variables = {
+      PUBLIC_KEY = var.public_key
+    }
+  }
 }
 
 resource "aws_iam_role" "lambda_exec" {
--- discord/variables.tf
+++ discord/variables.tf
@@ -2,3 +2,7 @@ variable "region" {
   description = "default aws region"
   default = "us-east-1"
 }
+
+variable "public_key" {
+  description = "pubkey of discord bot"
+}
