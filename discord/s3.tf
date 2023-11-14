ad9811a598e89359eeedee4446384d428739b10f
brooke-guajardo <brooke.guajardo@gmail.com>
2023-11-13 22:12:49 -0600

    python payload upload

--- discord/s3.tf
+++ discord/s3.tf
@@ -15,3 +15,19 @@ resource "aws_s3_bucket_acl" "lambda_bucket" {
   bucket = aws_s3_bucket.lambda_bucket.id
   acl    = "private"
 }
+
+data "archive_file" "lambda_load_python_artifact" {
+  type = "zip"
+
+  source_dir  = "${path.module}/deployment_package.zip"
+  output_path = "${path.module}/deployment_package.zip"
+}
+
+resource "aws_s3_object" "lambda_load_python_artifact" {
+  bucket = aws_s3_bucket.lambda_bucket.id
+
+  key    = "deployment_package.zip"
+  source = data.archive_file.lambda_load_python_artifact.output_path
+
+  etag = filemd5(data.archive_file.lambda_load_python_artifact.output_path)
+}
