7726cd080e40b27daaaac00a1a29861c99d4fc1e
brooke-guajardo <brooke.guajardo@gmail.com>
2023-11-15 19:37:54 -0600

    try dry

--- ecs/efs.tf
+++ ecs/efs.tf
@@ -20,21 +20,9 @@ resource "aws_efs_access_point" "minecraft_efs_ap" {
   }
 }
 
-# TODO: use a count or for loop to make this cleaner
-resource "aws_efs_mount_target" "minecraft_mount_1" {
-   file_system_id  = aws_efs_file_system.minecraft_efs.id
-   subnet_id = module.vpc.public_subnets[0]
-   security_groups = [aws_security_group.minecraft_efs_sg.id]
-}
-
-resource "aws_efs_mount_target" "minecraft_mount_2" {
-   file_system_id  = aws_efs_file_system.minecraft_efs.id
-   subnet_id = module.vpc.public_subnets[1]
-   security_groups = [aws_security_group.minecraft_efs_sg.id]
-}
-
-resource "aws_efs_mount_target" "minecraft_mount_3" {
-   file_system_id  = aws_efs_file_system.minecraft_efs.id
-   subnet_id = module.vpc.public_subnets[2]
-   security_groups = [aws_security_group.minecraft_efs_sg.id]
+resource "aws_efs_mount_target" "minecraft_mount" {
+  count            = length(module.vpc.public_subnets)
+  file_system_id   = aws_efs_file_system.minecraft_efs.id
+  subnet_id        = module.vpc.public_subnets[count.index]
+  security_groups  = [aws_security_group.minecraft_efs_sg.id]
 }
